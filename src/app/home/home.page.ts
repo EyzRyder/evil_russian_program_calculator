18adfc76c3ee60c41ad1b46f4b593f265fbec22f
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2023-12-23 23:33:59 -0300

    added calculation function

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  openModal: boolean = false;
+  maxPulUps: number = 0;
+  curWeek: 1 | 2 = 1;
 
-  constructor() {}
+  constructor(
+    public toastController: ToastController
+  ) { }
 
+  calculate() {
+    if (this.maxPulUps < 1 ) {
+      this.presentToast('Please input a valid value', 'danger');
+      return;
+    }
+    if (!(this.curWeek == 1 || this.curWeek == 2)) {
+      this.presentToast('Please input a valid value', 'danger');
+      return;
+    }
+    this.openModal = true;
+  }
+
+  async presentToast(msg: string, color: string) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 5000,
+      color: color
+    })
+    toast.present();
+  }
 }
